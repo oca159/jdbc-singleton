ae7aec0345ffdf671a25e6706d7868650cb4d689
maxence.derous <maxence.derous@C103FLIP05.btsigjb.local>
2012-10-11 12:26:07 +0200

    initial commit

---
+++ JavaLibrary4/src/JDBC.java
@@ -0,0 +1,87 @@
+/**
+ * Singleton for connecting to a database through JDBC
+ * 
+ * @author Maks
+ **/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC {
+    
+    private static Connection connection = null;
+    private final static String ADRESS   = "";
+    private final static String DATABASE = "";
+    private final static String USER     = "";
+    private final static String PASSWORD = "";
+    private final static String PORT     = "";
+    private final static String DRIVER   = "";
+    
+    /**
+     * Method that loads the specified driver
+     * 
+     * @return void
+     **/
+    
+    private static void loadDriver() {
+        try {
+            Class.forName(DRIVER);
+        }
+        catch (Exception e) {
+            errorHandler("Failed to load the driver " + DRIVER, e);
+        }
+    }
+
+    /**
+     * Method that loads the connection into the right property
+     * 
+     * @return void
+     **/
+    
+    private static void loadConnection() {
+        try {
+            connection = DriverManager.getConnection(getFormatedUrl(), USER, PASSWORD);
+        }
+        catch (SQLException e) {
+            errorHandler("Failed to connect to the database " + getFormatedUrl(), e);         
+        }
+    }
+    
+    /**
+     * Method that shows the errors thrown by the singleton
+     * 
+     * @param {String}    Message
+     * @param {Exception} e
+     * @return void
+     **/
+    
+    private static void errorHandler(String message, Exception e) {
+        System.out.println(message);  
+        System.out.println(e.getMessage());   
+    }
+
+    /**
+     * Method that returns the formated URL to connect to the database
+     * 
+     * @return {String}
+     **/
+    
+    private static String getFormatedUrl() {
+        return ADRESS + ":" + PORT + "/" + DATABASE;
+    }
+    
+    /**
+     * Static method that returns the instance for the singleton
+     * 
+     * @return {Connection} connection
+     **/
+    
+    public static Connection getConnection() {
+        if (connection == null) {
+            loadDriver();
+            loadConnection();
+        }
+        return connection;
+    }
+}
